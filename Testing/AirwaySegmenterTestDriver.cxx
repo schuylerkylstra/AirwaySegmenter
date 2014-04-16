a4b7be6763b14004b16cac767745846a1d7e16cf
Cory Quammen <cory.quammen@kitware.com>
2014-04-16 14:22:20 -0400

    Added comparison of test and baseline images

--- Testing/AirwaySegmenterTestDriver.cxx
+++ Testing/AirwaySegmenterTestDriver.cxx
@@ -2,12 +2,19 @@
 #include <iostream>
 #include <string>
 
+#include <itkImageFileReader.h>
+#include <itkTestingComparisonImageFilter.h>
+
 #include "itksys/Process.h"
 
+#if 0
 #define DEBUG(txt) \
   { \
   std::cout << txt << std::endl; \
   }
+#else
+#define DEBUG(txt)
+#endif
 
 // Finds the argument index that separates arguments
 // meant for the actual test and arguments meant
@@ -21,9 +28,9 @@ int SeparateArguments(int argc, char* const argv[])
       return i;
     }
   }
-  
+
   DEBUG("Leaving SeparateArguments");
-  
+
   return argc;
 }
 
@@ -39,11 +46,10 @@ int TestDriverInvokeProcess( int argc, char* argv[] )
   char **processArgv = new char*[argc];
   for ( int i = 1; i < argc; ++i ) {
     processArgv[i-1] = argv[i];
-    std::cout << processArgv[i-1] << std::endl;
   }
   processArgv[argc-1] = NULL;
   DEBUG("Done processing args");
-  
+
   itksysProcess *process = itksysProcess_New();
   itksysProcess_SetCommand(process, processArgv);
   itksysProcess_SetPipeShared(process, itksysProcess_Pipe_STDOUT, true);
@@ -51,9 +57,9 @@ int TestDriverInvokeProcess( int argc, char* argv[] )
   itksysProcess_Execute(process);
   itksysProcess_WaitForExit(process, NULL);
   delete[] processArgv;
-  
+
   DEBUG("Process launched");
-  
+
   int state = itksysProcess_GetState(process);
   switch ( state ) {
     case itksysProcess_State_Error:
@@ -63,14 +69,14 @@ int TestDriverInvokeProcess( int argc, char* argv[] )
       itksysProcess_Delete(process);
       return 1;
     }
-    
+
     case itksysProcess_State_Exception:
     {
       std::cerr << "AirwaySegmenterTestDriver: Process exception: "
         << itksysProcess_GetExceptionString(process) << std::endl;
       itksysProcess_Delete(process);
     }
-    
+
     case itksysProcess_State_Executing:
     {
       // this is not a possible state after itksysProcess_WaitForExit
@@ -110,12 +116,14 @@ int TestDriverInvokeProcess( int argc, char* argv[] )
       return 1;
     }
   }
-    
+
   int retCode = itksysProcess_GetExitValue(process);
   if ( retCode != 0 ) {
     std::cerr << "itkTestDriver: Process exited with return value: " << retCode << std::endl;
     }
   itksysProcess_Delete(process);
+
+  DEBUG("Leaving TestDriverInvokeProcess");
 
   return retCode;
 }
@@ -123,24 +131,60 @@ int TestDriverInvokeProcess( int argc, char* argv[] )
 int main(int argc, char* argv[])
 {
   int testArgc = SeparateArguments(argc, argv);
-  
+
   // Process --comparison options
-  std::string compareFile1;
-  std::string compareFile2;
+  std::string testFile;
+  std::string baselineFile;
   for ( int i = testArgc; i < argc; ++i ) {
     if ( strcmp( argv[i], "--compare" ) == 0 && i + 2 < argc ) {
-      compareFile1 = std::string( argv[i+1] );
-      compareFile2 = std::string( argv[i+2] );
+      testFile = std::string( argv[i+1] );
+      baselineFile = std::string( argv[i+2] );
       // Allow only two files to be compared at the moment
       break;
     }
   }
 
-  std::cout << "Comparing " << compareFile1 << " and " << compareFile2 << std::endl;
-
   // Launch the test command as a subprocess
   if ( TestDriverInvokeProcess( testArgc, argv ) != EXIT_SUCCESS ) {
-    std::cout << "Failed to invoke test process " << argv[1] << std::endl;
+    std::cerr << "Failed to invoke test process " << argv[1] << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  if ( testFile.empty() || baselineFile.empty() ) {
+    std::cout << "testFile: " << testFile << std::endl;
+    std::cout << "baselineFile: " << baselineFile << std::endl;
+    return EXIT_SUCCESS;
+  }
+
+  // Read in comparison images and check that they match
+  typedef float                              PixelType;
+  const unsigned int                         Dimension = 3;
+  typedef itk::Image< PixelType, Dimension > ImageType;
+  typedef itk::ImageFileReader< ImageType >  ImageReaderType;
+
+  ImageReaderType::Pointer testReader = ImageReaderType::New();
+  testReader->SetFileName( testFile.c_str() );
+
+  ImageReaderType::Pointer baselineReader = ImageReaderType::New();
+  baselineReader->SetFileName( baselineFile.c_str() );
+
+  typedef itk::Testing::ComparisonImageFilter< ImageType, ImageType > ComparisonFilterType;
+  ComparisonFilterType::Pointer comparisonFilter = ComparisonFilterType::New();
+  comparisonFilter->SetTestInput( testReader->GetOutput() );
+  comparisonFilter->SetValidInput( baselineReader->GetOutput() );
+  try {
+    comparisonFilter->UpdateLargestPossibleRegion();
+
+    ComparisonFilterType::AccumulateType totalImageDifference =
+      comparisonFilter->GetTotalDifference();
+    std::cout << "Total image difference: " << totalImageDifference << std::endl;
+
+    if ( totalImageDifference > itk::NumericTraits< ComparisonFilterType::AccumulateType >::ZeroValue() ) {
+      std::cerr << "Image mismatch between test image and baseline\n";
+      return EXIT_FAILURE;
+    }
+  } catch ( itk::ExceptionObject & except ) {
+    std::cerr << except << std::endl;
     return EXIT_FAILURE;
   }
 
