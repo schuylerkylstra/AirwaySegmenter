8e0d371884eb3fe0bd77af67b89ea61bc08d3cb2
Jonathan Kylstra <jskylstr@cs.unc.edu>
2015-04-17 15:25:31 -0400

    Moved the StoreArgs function to a new file

--- AirwaySegmenter.hxx
+++ AirwaySegmenter.hxx
@@ -56,73 +56,9 @@
 #include "FastMarchIt.hxx"
 #include "LabelIt.hxx"
 #include "ProgramArguments.h"
+#include "AirwayWriteArgs.h"
 
 namespace AirwaySegmenter {
-  void StoreArgs( const ProgramArguments & args )
-  {
-    std::ofstream argsFile;
-
-    argsFile.open(args.argsFile.c_str()); 
-
-    
-    argsFile << " --createOutputGeometry";
-
-    argsFile << " --lowerSeed " << args.lowerSeed[0] << ","
-                                << args.lowerSeed[1] << ","
-                                << args.lowerSeed[2];
-    argsFile << " --lowerSeedRadius " << args.lowerSeedRadius;
-
-    argsFile << " --upperSeed " << args.upperSeed[0] << ","
-                                << args.upperSeed[1] << ","
-                                << args.upperSeed[2] ;
-    argsFile << " --upperSeedRadius " << args.upperSeedRadius;
-
-    if (args.airwayFragmentSeeds.size()){
-      argsFile << " --addAirwayFragments";
-    }
-    for (size_t i = 0; i < args.airwayFragmentSeeds.size(); ++i) {
-      argsFile << " --airwayFragmentSeed "
-        << args.airwayFragmentSeeds[i][0] << ","
-        << args.airwayFragmentSeeds[i][1] << ","
-        << args.airwayFragmentSeeds[i][2];
-    }
-
-    if (args.trachealTubeSeed.size() >= 3) {
-      argsFile << " --trachealTubeSeed " << args.trachealTubeSeed[0] << ","
-                                         << args.trachealTubeSeed[1] << ","
-                                         << args.trachealTubeSeed[2];
-      argsFile << " --trachealTubeSeedRadius " << args.trachealTubeSeedRadius;
-    }
-    
-
-    if (args.bRemoveMaxillarySinuses) {
-      argsFile << " --removeMaxillarySinuses";
-    }
-    for (size_t i = 0; i < args.maxillarySinusesSeeds.size(); ++i) {
-      argsFile << " --maxillarySinusesSeed "
-        << args.maxillarySinusesSeeds[i][0] << ","
-        << args.maxillarySinusesSeeds[i][1] << ","
-        << args.maxillarySinusesSeeds[i][2];
-    }
-    argsFile << " --maxillarySinusesSeedsRadius " << args.maxillarySinusesSeedsRadius;
-
-    argsFile << " --erosionPercentage " << args.erosionPercentage;
-    argsFile << " --maxAirwayRadius " << args.dMaxAirwayRadius;
-    argsFile << " --erodeDistance " << args.dErodeDistance;
-    argsFile << " --component " << args.iComponent;
-    
-    if(args.bRemoveBreathingMask){
-      argsFile << " --breathingMaskThickness " << args.dBreathingMaskThickness;
-    }
-    
-    argsFile << " --noWarning";
-    if(args.bRAIImage){
-      argsFile << " --RAIImage";
-      argsFile << " --RAIImagePath " << args.sRAIImagePath;
-    }
-
-    argsFile.close();
-  }
 
   /*******************************************************************/
   /** Run the algorithm on an input image and write it to the output
---
+++ Code/AirwayWriteArgs.h
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+#include <fstream>
+#include <iostream>
+#include "ProgramArguments.h"
+
+#ifndef AirwaySegmenter_AirwayWriteArgs_h_included
+#define AirwaySegmenter_AirwayWriteArgs_h_included
+
+namespace AirwaySegmenter {
+
+  void StoreArgs( const ProgramArguments & args );
+
+}
+
+#endif
